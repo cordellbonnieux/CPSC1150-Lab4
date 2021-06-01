55412a2e18cfb3efd6a4ced4a123e578305b1af6
Cordell <cordellbonnieux@gmail.com>
2021-06-01 08:14:06 -0700

    trying recursive method to handle user inputs.

--- RSPGame.java
+++ RSPGame.java
@@ -10,10 +10,34 @@ import java.util.Scanner;
 public class LogicalOps {
     public static void main(String[] args){
 
-        //
+        // create scanner object
         Scanner input = new Scanner(System.in);
 
-        //
+        // prompt the user to enter 0, 1 or 2
+        System.out.println("Select: rock (0), scissors (1), or paper (2)");
 
+        userPlay(input.nextInt(), input);
+
+
+    }
+    public static int userPlay(input.nextInt(), input) {
+
+        int output;
+
+        try {
+
+            output = userInput;
+
+        } catch (InputMismatchException a) {
+
+            // tell the user to try a new input
+            System.out.println("Invalid entry, please enter: rock (0), scissors (1), or paper (2)");
+
+            // restart the method
+            userPlay(input.nextInt(), input);
+
+        }
+
+        return output;
     }
 }
